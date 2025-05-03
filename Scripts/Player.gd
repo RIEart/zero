7aca93e35789a285f90ab8d5eaa39de8473030fd
sulamita-barbosa <sulakit75.sb@gmail.com>
2025-05-03 13:33:38 -0300

    add arquivos ao git

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,12 +16,13 @@ func _physics_process(delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	
 	is_grounded = _check_is_ground()
+	
+	_set_animation()
 
 func _get_input():
+	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"));
-	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2);
-	
-	
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
 	
 	if move_direction != 0:
 		$texture.scale.x = move_direction
@@ -36,4 +37,15 @@ func _check_is_ground():
 			return true
 			
 	return false
+	
+func _set_animation():
+	var anim = "idle"
+	
+	if !is_grounded:
+		anim = "jump"
+	elif velocity.x != 0:
+		anim = "run"
+	
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
 	
