39ff82690c59988ac569a7f0b395eb0fe54e66be
RIEart <jesusarthursiqueira@gmail.com>
2025-05-31 15:06:04 -0300

    aula de fruitas

---
+++ apple.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+func _ready():
+	pass
+						 
+
+func _on_apple_body_entered(body):
+	print(body.name)
+	$anim.play("collected")
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "collected":
+		queue_free()
