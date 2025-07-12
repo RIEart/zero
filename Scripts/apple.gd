5ef260d94de79785ad2ff559bdd7ff807808b371
dev <dev@dev.com>
2025-07-12 15:14:29 -0300

    Falling Platformer

---
+++ Scripts/apple.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _on_items_body_entered(body):
+	print(body.name)
+	$anim.play("collected")
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "collected":
+		queue_free()
