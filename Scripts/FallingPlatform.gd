6d288a5fa76de95c3587074d15ccbfc920d69098
RIE_art <jesusarthursiqueira@gmail.com>
2025-07-26 15:04:40 -0300

    PLATAFORMA 2

--- Scripts/FallingPlatform.gd
+++ Scripts/FallingPlatform.gd
@@ -7,7 +7,7 @@ onready var timer = $timer
 onready var reset_position = global_position
 
 var velocity = Vector2.ZERO
-var gravity = 720 
+var gravity = 200
 var is_triggered = false
 export var reset_timer = 3.0
 
@@ -17,6 +17,12 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	position += velocity * delta
+	
+func collide_with(_collision: KinematicCollision2D, _collider: KinematicBody2D):
+	if !is_triggered:
+		is_triggered = true
+		anim.play("shake")
+		velocity = Vector2.ZERO 
 
 
 
@@ -2260,3 +2266,19 @@ func _physics_process(delta: float) -> void:
 
 
 
+
+
+func _on_anim_animation_finished(anim_name):
+	set_physics_process(true)
+	timer.start(reset_timer)
+
+
+func _on_timer_timeout():
+	set_physics_process(false)
+	yield(get_tree(), "physics_frame")
+	var temp = collision_layer
+	collision_layer= 0
+	global_position = reset_position
+	yield(get_tree(), "physics_frame")
+	collision_layer = temp
+	is_triggered = false 
